7f4d6ec5c0795f1279140bf51ac46c9721f56f94
hemmy3103 <ramananjatohemmy@gmail.com>
2023-04-04 16:23:50 +0300

    Add template in exercice 4

---
+++ src/main/java/jsf/BeanTemplate.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author h.ramananjato
+ */
+@Named(value = "beanTemplate")
+@RequestScoped
+public class BeanTemplate {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    public BeanTemplate() {
+    }
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage__4?nb=" + nombre + "&faces-redirect=true";
+    }
+    
+    public String displayPrevious() {
+        return "formulaire__4?nb=" + nombre + "&faces-redirect=true";
+    }
+}
